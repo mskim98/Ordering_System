abc494b44478a068501264a377fcdb54fcc315e1
mskim <msung98@naver.com>
2025-04-29 15:43:55 +0900

    bug : decorator 수정

--- backend/src/auth/decorator/rbac.decorator.ts
+++ backend/src/auth/decorator/rbac.decorator.ts
@@ -1,6 +1,4 @@
-import { SetMetadata } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Permission } from '../permission/permission';
 
-export const RBAC_KEY = 'permissions';
-export const RBAC = (permissions: Permission[]) =>
-  SetMetadata(RBAC_KEY, permissions);
+export const RBAC = Reflector.createDecorator<Permission[]>();
